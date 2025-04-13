0c3bb4e4dff57acf5e2c0075b7c9ff0499f41545
Francis Batista <francisbatista2@gmail.com>
2025-04-13 16:44:46 -0400

    Initial Commit

---
+++ JsonPlaceholderTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JsonPlaceholderTestApp
+//
+//  Created by Francis Batista on 13/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JsonPlaceholderTestApp/JsonPlaceholderTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonPlaceholderTestAppApp.swift
+//  JsonPlaceholderTestApp
+//
+//  Created by Francis Batista on 13/4/25.
+//
+
+import SwiftUI
+
+@main
+struct JsonPlaceholderTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
